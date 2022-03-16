d965ab17582ac4a9ab551faca0b5ccaecb3e1e0d
Tadas Cislikauskas <tacista@gmail.com>
2022-03-16 16:07:34 +0200

    Almost final version. Needs to be added user interface quality of life improvements when inputing wrong currency hashmap key numbers in the scanner.

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -5,49 +5,61 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class CurrencyConverter {
 
 
-    public static void main(String[] args) {
-        HashMap<Integer, String> currencyCode = new HashMap<>();
+    public static void main(String[] args) throws IOException {
+        Boolean running = true;
+        do {
+            HashMap<Integer, String> currencyCode = new HashMap<>();
 
 //  Adding currency codes to recognise certain currency
-        currencyCode.put(1, "EUR");
-        currencyCode.put(2, "USD");
-        currencyCode.put(3, "RUB");
-        currencyCode.put(4, "ZLT");
-        currencyCode.put(5, "JUAN");
+            currencyCode.put(1, "EUR");
+            currencyCode.put(2, "USD");
+            currencyCode.put(3, "RUB");
+            currencyCode.put(4, "PLN");
+            currencyCode.put(5, "CNY");
 
 
-        String fromCode;
-        String toCode;
-        double amount;
+            String fromCode;
+            String toCode;
+            double amount;
 
 //  Scanner for the inputs
-        Scanner sc = new Scanner(System.in);
-        System.out.println("CURRENCY CONVERTER");
+            Scanner sc = new Scanner(System.in);
+            System.out.println("CURRENCY CONVERTER");
 
-        System.out.println("What currency are you converting FROM?");
-        System.out.println("Enter the number for: ");
-        System.out.println("1: euros\n 2: US dollars\n 3: russian rubles\n 4: polish zloty\n 5: chinese juan");
-        fromCode = currencyCode.get(sc.nextInt());
+            System.out.println("What currency are you converting FROM?");
+            System.out.println("Enter the number for: ");
+            System.out.println(" 1: Euros\n 2: US Dollars\n 3: Russian Rubles\n 4: Polish Zloty\n 5: Chinese Renminbi");
+            fromCode = currencyCode.get(sc.nextInt());
 
-        System.out.println("What currency are you converting TO?");
-        System.out.println("Enter the number for: ");
-        System.out.println("1: euros\n 2: US dollars\n 3: russian rubles\n 4: polish zloty\n 5: chinese juan");
-        toCode = currencyCode.get(sc.nextInt());
+            System.out.println("What currency are you converting TO?");
+//        System.out.println("Enter the number for: ");
+            System.out.println(" 1: Euros\n 2: US Dollars\n 3: Russian Rubles\n 4: Polish Zloty\n 5: Chinese Renminbi");
+            toCode = currencyCode.get(sc.nextInt());
 
-        System.out.println("What is the amount you wish to convert?");
-        amount = sc.nextDouble();
+            System.out.println("What is the amount you wish to convert?");
+            amount = sc.nextDouble();
 
 //  getting the exchange rate from API
-//        sendHttpRequest (fromCode, toCode, amount);
+            sendHttpRequest(fromCode, toCode, amount);
+            System.out.println("Do you want to convert one more time?");
+            System.out.println("Press 1 for YES \t Press any other number for NO");
+            if (sc.nextInt() != 1) {
+                running = false;
+            }
+
+        } while (running);
+        System.out.println("Thank you for using currency converter!");
     }
 
     private static void sendHttpRequest(String fromCode, String toCode, double amount) throws IOException {
+        DecimalFormat df = new DecimalFormat("00.00");
         String getURL = "https://api.exchangerate.host/latest?base=" + toCode + "&symbols=" + fromCode;
         URL url = new URL(getURL);
         HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
@@ -70,11 +82,13 @@ public class CurrencyConverter {
             JSONObject jsonObject = new JSONObject(response.toString());
             Double exchangeRate = jsonObject.getJSONObject("rates").getDouble(fromCode);
 
-
+//            System.out.println(jsonObject.getJSONObject("rates"));
+            System.out.println("Exchange rate is: " + exchangeRate);
+            System.out.println(df.format(amount) + " " + fromCode + " = " + df.format(amount / exchangeRate) + " " + toCode);
+        } else {
+            System.out.println("Failed request");
         }
 
 
     }
-
-
 }
